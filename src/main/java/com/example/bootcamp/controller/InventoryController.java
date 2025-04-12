4ac174b052822a1984ad76b62aaa18f2a17f092a
Shiv991319 <shivanshu.mishra@olx.in>
2025-04-13 02:28:34 +0530

    Add controller to get inventory by Sku

--- src/main/java/com/example/bootcamp/controller/InventoryController.java
+++ src/main/java/com/example/bootcamp/controller/InventoryController.java
@@ -19,5 +19,12 @@ public class InventoryController {
         return new ResponseEntity<>(createdInventory, HttpStatus.CREATED);
     }
 
-
+    @GetMapping("/{sku}")
+    public ResponseEntity<?> getInventory(@PathVariable String sku) {
+        Inventory inventory=inventoryService.getInventoryBySku(sku);
+        if(inventory==null) {
+            return new ResponseEntity<>("Inventory not found",HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(inventory, HttpStatus.OK);
+    }
 }
