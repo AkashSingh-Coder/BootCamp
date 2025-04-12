529456a7d38e9d1f2bb6bf81b803bbd432f4232b
AkashSingh-Coder <akash.singh1@olx.in>
2025-04-12 17:26:28 +0530

    adding Repository

---
+++ src/main/java/com/example/bootcamp/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.bootcamp.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.bootcamp.Entity.Inventory;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    
+}
