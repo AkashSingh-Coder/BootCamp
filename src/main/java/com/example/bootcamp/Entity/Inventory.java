d6dd3f09870106ad3840a94e47bc4085f5999cfb
Shiv991319 <shivanshu.mishra@olx.in>
2025-04-12 16:21:17 +0530

    Create representation of database table

---
+++ src/main/java/com/example/bootcamp/Entity/Inventory.java
@@ -0,0 +1,35 @@
+package com.example.bootcamp.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+public class Inventory {
+
+    @Id
+    private String sku;
+    private String type;
+    private String status;
+    private String primaryLocation;
+
+    private String vin;
+    private String make;
+    private String model;
+    private String trim;
+    private int year;
+
+    private double costPrice;
+    private double sellPrice;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private String createdBy;
+    private String updatedBy;
+
+}
