564ce77eb64f5fdec434da7469cbbabbd2dc0898
AkashSingh-Coder <akash.singh1@olx.in>
2025-04-13 00:43:20 +0530

    Adding package and import in InventoryService

---
+++ src/main/java/com/example/bootcamp/service/InventoryService.java
@@ -0,0 +1,12 @@
+package com.example.bootcamp.service;
+
+import com.example.bootcamp.repository.InventoryRepository;
+import com.example.bootcamp.Entity.Inventory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
