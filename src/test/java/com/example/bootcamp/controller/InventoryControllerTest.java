b3353902ea5c19a6ed24ccc77299b597d40c5b4f
Shiv991319 <shivanshu.mishra@olx.in>
2025-04-13 19:11:25 +0530

    Add test for createInventory controller

---
+++ src/test/java/com/example/bootcamp/controller/InventoryControllerTest.java
@@ -0,0 +1,66 @@
+package com.example.bootcamp.controller;
+
+import com.example.bootcamp.Entity.Inventory;
+import com.example.bootcamp.enums.InventoryStatus;
+import com.example.bootcamp.service.InventoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class InventoryControllerTest {
+    @Mock
+    private InventoryService inventoryService;
+
+    @InjectMocks
+    private InventoryController inventoryController;
+    private Inventory inventory;
+
+    @BeforeEach
+    void setUp() {
+        Inventory inventory = new Inventory();
+        inventory.setSku("12345");
+        inventory.setType("Car");
+        inventory.setStatus(InventoryStatus.CREATED);
+        inventory.setPrimaryLocation("Gurugram, Haryana");
+        inventory.setVin("1HGCM82633A123456");
+        inventory.setMake("Honda");
+        inventory.setModel("Civic");
+        inventory.setTrim("EX");
+        inventory.setYear(2022);
+        inventory.setCostPrice(20000.0);
+        inventory.setSellingPrice(25000.0);
+        inventory.setCreatedAt(LocalDateTime.now());
+        inventory.setUpdatedAt(LocalDateTime.now());
+        inventory.setCreatedBy("Admin");
+        inventory.setUpdatedBy("Admin");
+    }
+
+    @Test
+    void createInventory() {
+        Inventory inventory = new Inventory();
+        when(inventoryService.createInventory(eq(inventory))).thenReturn(inventory);
+
+        ResponseEntity<Inventory> response = inventoryController.createInventory(inventory);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(inventory, response.getBody());
+        verify(inventoryService, times(1)).createInventory(eq(inventory));
+    }
+
+
+
+
+
+}
