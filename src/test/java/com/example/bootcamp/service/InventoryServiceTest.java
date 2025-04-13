25c1e4606582a891f8c24fa119e8f938d622ab42
AkashSingh-Coder <akash.singh1@olx.in>
2025-04-13 19:23:54 +0530

    get all Inventory

--- src/test/java/com/example/bootcamp/service/InventoryServiceTest.java
+++ src/test/java/com/example/bootcamp/service/InventoryServiceTest.java
@@ -8,9 +8,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -63,10 +67,32 @@ class InventoryServiceTest {
 
     @Test
     void getAllInventories() {
+        Inventory inventory1 = new Inventory();
+        inventory1.setSku("12345");
+        inventory1.setSellingPrice(1000.0);
+
+        Inventory inventory2 = new Inventory();
+        inventory2.setSku("67890");
+        inventory2.setSellingPrice(2000.0);
+
+        when(inventoryRepository.findAll(PageRequest.of(0, 10)))
+                .thenReturn(new PageImpl<>(List.of(inventory1, inventory2)));
+
+        List<Inventory> result = inventoryService.getAllInventories(0, 10);
+
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("12345", result.get(0).getSku());
+        assertEquals("67890", result.get(1).getSku());
+
+
+        verify(inventoryRepository, times(1)).findAll(PageRequest.of(0, 10));
     }
 
     @Test
     void updateInventory() {
+
     }
 
     @Test
